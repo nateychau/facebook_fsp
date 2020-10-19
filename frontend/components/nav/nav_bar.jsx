a3963f3eccffbcc0df63e5e22e1e5fd390994f16
Nathan Chau <nateychau@gmail.com>
2020-10-18 21:09:42 -0700

    Add icons for nav bar icons, add messages and notif containers

--- frontend/components/nav/nav_bar.jsx
+++ frontend/components/nav/nav_bar.jsx
@@ -4,11 +4,31 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends React.Component {
     constructor(props){
         super(props);
+        this.state={
+            messages: false,
+            notif: false,
+            logout: false
+        }
         this.handleLogout = this.handleLogout.bind(this);
+        this.handleOpenDropdown = this.handleOpenDropdown.bind(this);
+        this.handleCloseDropwdown = this.handleCloseDropdown.bind(this);
     }
 
     handleLogout(){
+        console.log('event fired')
         this.props.logout();
+    }
+
+    handleOpenDropdown(type){
+        return (e) => {
+            this.setState({[type]: true})
+        }
+    }
+
+    handleCloseDropdown(type){
+        return (e) => {
+            this.setState({[type]: false})
+        }
     }
 
     render(){
@@ -23,8 +43,28 @@ export default class Navbar extends React.Component {
                     <a href="https://nateychau.github.io/" target="_blank"><i className="fas fa-address-card"></i></a>
                 </div>
                 <div className="nav-right">
-                <Link to={`/users/${this.props.currentUser.id}`}><button className="btn profile">{this.props.currentUser.first_name}</button></Link>
-                    <button className="btn" onClick={this.handleLogout}>Logout</button>
+                    <Link to={`/users/${this.props.currentUser.id}`}>
+                        <button className="btn-profile">
+                            <div className="prof-pic-thumb-smallest">
+                                <img src={this.props.currentUser.profile_photo}></img>
+                            </div>
+                            {this.props.currentUser.first_name}
+                        </button>
+                    </Link>
+                    <button id={this.state.messages ? 'active-nav-button' : ''} onClick={this.handleOpenDropdown("messages")} onBlur={this.handleCloseDropwdown("messages")} className="util-btn"><div className="messages"></div></button>
+                    {this.state.messages ? <div className="util-container">
+                        <div>Messenger</div>
+                        <div>You have no new messages</div>
+                    </div> : <></>}
+                    <button id={this.state.notif ? 'active-nav-button' : ''} onClick={this.handleOpenDropdown("notif")} onBlur={this.handleCloseDropwdown("notif")} className="util-btn"><div className="notifications"></div></button>
+                    {this.state.notif ? <div className="util-container">
+                        <div>Notifications</div>
+                        <div>You have no new notifications</div>
+                    </div> : <></>}
+                    <button id={this.state.logout ? 'active-nav-button' : ''} onClick={this.handleOpenDropdown("logout")} onBlur={this.handleCloseDropwdown("logout")} className="util-btn"><div className="dropdown"></div></button>
+                    {this.state.logout ? <div className="util-container">
+                        <button onMouseDown={this.handleLogout} className="logout-btn"><div className="icon-container"><div className="logout-icon"></div></div><div>Log out</div></button>
+                    </div> : <></>}
                 </div>
             </div>
         )
