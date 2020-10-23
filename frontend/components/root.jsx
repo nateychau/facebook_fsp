38d008f979cf5a3c9a6f6d75d1b0383a5d2bf7cc
Nathan Chau <nateychau@gmail.com>
2020-10-23 11:01:07 -0700

    add scroll reset functionality

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import ScrollToTop from './nav/scroller';
 
 const Root = ({ store }) => (
   <Provider store={store}>
       <HashRouter>
+        <ScrollToTop>
           <App />
+        </ScrollToTop>
       </HashRouter>
   </Provider>
 );
