e43dbcc79c90c37b1692a5b61a4570bd4c3005d8
Nathan Chau <nateychau@gmail.com>
2020-11-10 17:46:22 -0800

    Add email validation gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
     validates :email, :session_token, presence: true, uniqueness: true
     validates :first_name, :last_name, :password_digest, presence: true
     validates :password, length: { minimum: 6, allow_nil: true }
+    validates :email, 'valid_email_2/email': true
 
     attr_reader :password
     after_initialize :ensure_session_token
